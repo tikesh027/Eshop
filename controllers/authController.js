11fa9546f4ec67913227645d2cc29bc70fcd4f24
tikesh027 <tikeshsingh027@yahoo.com>
2022-11-28 20:41:40 +0530

    Created API to sign up user

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+
+exports.signup = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const mappedError = errors.array();
+        if(mappedError.length === 1){
+            res.status(400).json(mappedError[0].msg);
+            return;
+        }
+        const errorObject = {}
+        mappedError.forEach((error)=>{
+            errorObject[error.param] = error.msg;
+        })
+        res.status(400).json(errorObject);
+        return;
+    }
+
+    const { email, password, firstName, lastName, contactNumber } = req.body;
+
+    User.findOne({ email: email })
+        .then((result) => {
+            if(result){
+                res.status(400).json('Try any other email, this email is already registered!');
+                return;
+            }
+        }).catch((error)=>{
+            console.log(error);
+            res.status(500).json('Internal Server Error');
+            return;
+        });
+    
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+        email,
+        contactNumber,
+        firstName,
+        password: hashedPassword,
+        role: 'USER',
+        lastName: lastName,
+        userName: ''
+    });
+
+    newUser.save().then((result) => {
+        const response = {
+            _id: result._id,
+            firstName: result.firstName,
+            lastName: result.lastName,
+            email: result.email
+        }
+        res.status(200).json(response);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).json('Internal server error');
+    })
+
+}
+
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { body } = require('express-validator/check');
+const AuthController = require('../controllers/authController');
+const router = express.Router();
+
+router.post('/users', [
+    body('email').isEmail().withMessage('Invalid email-id format!'),
+    body('contactNumber').trim().isNumeric().isLength({ max: 10, min: 10 }).withMessage('Invalid contact number!'),
+    body('password').trim().isLength({ min: 2 }).withMessage('Password too short')
+], AuthController.signup);
+
+
+module.exports = router;
